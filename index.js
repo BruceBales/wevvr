ac58905167bc7ce78269013dd11c901e69a744e6
Bruce Bales <bbales@kochava.com>
2017-04-15 01:22:21 -0700

    Change config to be read as an array, in preparation to implement RethinkDB

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ var appKey = "Dd609af98cDfCdae7C8A04c4Bb5eC3Ee";
 var channel = "NWS-All-USA-Alerts";
 
 //Will eventually be pulled from a database, to allow front-end web modification
-var conf = {
-  "email": "bruce.morgan.bales@gmail.com",
-  "criteria": {
-    "msgType": "Alert",
-  }
-}
+var conf = [
+    {
+        "email": "bruce.morgan.bales@gmail.com",
+        "criteria": {
+            "msgType": "Alert"
+        }
+    },
+    {
+        "email": "test@example.com",
+        "criteria": {
+            "msgType": "Alert"
+        }
+    },
+    {
+        "email": "test2@example.com",
+        "criteria": {
+            "msgType": "Alert"
+        }
+    }
+]
 
 var rtm = new RTM(endpoint, appKey);
 rtm.on("enter-connected", function() {
@@ -23,8 +37,11 @@ rtm.on("enter-connected", function() {
 var subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE);
 subscription.on('rtm/subscription/data', function (pdu) {
   pdu.body.messages.forEach(function (msg) {
-    var al = process.Alert(conf, msg);
-    process.Mail(al, conf.email, msg);
+    for (var i in conf) {
+      var al = process.Alert(conf[i], msg);
+      process.Mail(al, conf[i].email, msg);
+      console.log("Sent mail to" + conf[i].email);
+    }
   });
 });
 
