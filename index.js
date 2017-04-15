c3d75d0ebe79bc16d63be4d9d5d50140934f84b3
Bruce Bales <bbales@kochava.com>
2017-04-15 03:00:01 -0700

    Add RethinkDB as source of user config array

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 var RTM = require("satori-sdk-js");
 var process = require("./lib/process.js");
-
-
+var r = require('rethinkdbdash')();
 
 var endpoint = "wss://open-data.api.satori.com";
 var appKey = "Dd609af98cDfCdae7C8A04c4Bb5eC3Ee";
 var channel = "NWS-All-USA-Alerts";
 
-//Will eventually be pulled from a database, to allow front-end web modification
-var conf = [
-    {
-        "email": "bruce.morgan.bales@gmail.com",
-        "criteria": {
-            "msgType": "Alert"
-        }
-    },
-    {
-        "email": "test@example.com",
-        "criteria": {
-            "msgType": "Alert"
-        }
-    },
-    {
-        "email": "test2@example.com",
-        "criteria": {
-            "msgType": "Alert"
-        }
-    }
-]
 
 var rtm = new RTM(endpoint, appKey);
 rtm.on("enter-connected", function() {
@@ -37,11 +15,14 @@ rtm.on("enter-connected", function() {
 var subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE);
 subscription.on('rtm/subscription/data', function (pdu) {
   pdu.body.messages.forEach(function (msg) {
-    for (var i in conf) {
-      var al = process.Alert(conf[i], msg);
-      process.Mail(al, conf[i].email, msg);
-      console.log("Sent mail to" + conf[i].email);
-    }
+    r.table('Users').run().then(function(result) {
+      var conf = result;
+      for (var i in conf) {
+        var al = process.Alert(conf[i], msg);
+        process.Mail(al, conf[i].email, msg);
+        console.log("Sent mail to" + conf[i].email);
+      }
+    });
   });
 });
 
